0318ffc54ca82fcfe53559e167709b43ca1d0bcb
maggithor97 <maggithor1@gmail.com>
2021-10-25 20:33:14 +0000

    UserController grunnur og login/signup grunnar

--- src/main/java/is/hi/hbv501g2021supportsession/Services/Implementation/UserServiceImplementation.java
+++ src/main/java/is/hi/hbv501g2021supportsession/Services/Implementation/UserServiceImplementation.java
@@ -1,6 +1,44 @@
 package is.hi.hbv501g2021supportsession.Services.Implementation;
 
+import is.hi.hbv501g2021supportsession.Persistence.Entities.Book;
+import is.hi.hbv501g2021supportsession.Persistence.Repositories.BookRepository;
 import is.hi.hbv501g2021supportsession.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class UserServiceImplementation implements UserService {
+    private BookRepository bookRepository;
+
+    @Autowired
+    public UserServiceImplementation(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // Todo: substitute bookRepository for userRepository when it is ready
+    @Override
+    public Book saveUser(Book user) {
+        return bookRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Book user) {
+        bookRepository.delete(user);
+    }
+
+    @Override
+    public List<Book> findUserByName(String name) {
+        return bookRepository.findByTitle(name);
+    } // Todo: substitute with findByName when it exists
+
+    @Override
+    public Book loginUser(Book user) {
+        return bookRepository.save(user);
+    } // substitute save with login
+
+    @Override
+    public Book logoutUser(Book user) {
+        return bookRepository.save(user);
+    } // substitute save with logout
+
 }
