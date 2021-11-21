92594165801b78d908926f6ce6face5a508fc2b3
tfj2 <tfj2@hi.is>
2021-11-21 15:43:59 +0000

    data

--- src/main/java/is/hi/hbv501g2021supportsession/Persistence/Entities/LoginInfo.java
+++ src/main/java/is/hi/hbv501g2021supportsession/Persistence/Entities/LoginInfo.java
@@ -6,31 +6,39 @@ import javax.persistence.*;
 @Table(name = "loginInfo")
 public class LoginInfo {
     private String password;
+    private byte[] salt;
     @Id //ekki GUID, hvernig ættum við að græja það
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long ID;
 
-    public LoginInfo(String password, long ID) {
-        this.password = password;
-        this.ID = ID;
-    }
+
+    public LoginInfo(String password, long ID, byte[] salt) {
+            this.password = password;
+            this.ID = ID;
+            this.salt = salt;
+        }
 
     public LoginInfo() {
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public long getID() {
+            return ID;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public void setID(long ID) {
+            this.ID = ID;
+        }
+
+        public byte[] getSalt() { return salt;}
+
+        public void setSalt(byte[] salt) {this.salt = salt;}
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public long getID() {
-        return ID;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setID(long ID) {
-        this.ID = ID;
-    }
-}
