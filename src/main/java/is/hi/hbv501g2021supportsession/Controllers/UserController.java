9e4dc76cbbc360f19b6a012d55536f54059cb948
maggithor97 <maggithor1@gmail.com>
2021-11-24 14:59:56 +0000

    Add/workouts-inserted-into-user-at-signup

--- src/main/java/is/hi/hbv501g2021supportsession/Controllers/UserController.java
+++ src/main/java/is/hi/hbv501g2021supportsession/Controllers/UserController.java
@@ -1,7 +1,9 @@
 package is.hi.hbv501g2021supportsession.Controllers;
 
 import is.hi.hbv501g2021supportsession.Persistence.Entities.User;
+import is.hi.hbv501g2021supportsession.Persistence.Entities.Workout;
 import is.hi.hbv501g2021supportsession.Services.UserService;
+import is.hi.hbv501g2021supportsession.Services.WorkoutService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,16 +13,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 
 @Controller
 public class UserController {
 
     UserService userService;
+    WorkoutService workoutService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, WorkoutService workoutService) {
         this.userService = userService;
+        this.workoutService = workoutService;
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
@@ -84,6 +89,17 @@ public class UserController {
         }
         System.out.println("Username: " + user.getName());
         System.out.println("Password: " + user.getLoginInfo().getPassword());
+        System.out.println("Difficulty: " + user.getUserFitnessInfo().getDifficulty().toString());
+
+
+        if(user.getUserFitnessInfo() != null) {
+            List<Workout> allWorkouts = workoutService.findAll();
+            System.out.println(allWorkouts.get(0).getWorkoutName());
+            user.getUserFitnessInfo().setWorkouts(allWorkouts);
+        }
+
+        System.out.println("Workout 0: " + user.getUserFitnessInfo().getWorkouts().get(0).getWorkoutName());
+        System.out.println("Workout 1: " + user.getUserFitnessInfo().getWorkouts().get(1).getWorkoutName());
 
         User exists = userService.findUserByName(user.getName());
         // Catch error of username already exists
