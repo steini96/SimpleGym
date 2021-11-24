1a6d00564bd92219859a7de114d0938431d3aa50
steini96 <steini.tki@gmail.com>
2021-11-24 13:39:33 +0000

    Creating the workout endpoint and site.

--- src/main/java/is/hi/hbv501g2021supportsession/Controllers/WorkoutController.java
+++ src/main/java/is/hi/hbv501g2021supportsession/Controllers/WorkoutController.java
@@ -1,8 +1,6 @@
 package is.hi.hbv501g2021supportsession.Controllers;
 
-import is.hi.hbv501g2021supportsession.Persistence.Entities.User;
-import is.hi.hbv501g2021supportsession.Persistence.Entities.UserFitnessInfo;
-import is.hi.hbv501g2021supportsession.Persistence.Entities.Workout;
+import is.hi.hbv501g2021supportsession.Persistence.Entities.*;
 import is.hi.hbv501g2021supportsession.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import is.hi.hbv501g2021supportsession.Services.WorkoutService;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -55,9 +54,33 @@ public class WorkoutController {
         return "redirect:/";
     }
 
-    @RequestMapping(value = "/work2", method = RequestMethod.GET)
+    @RequestMapping(value = "/getWorkout", method = RequestMethod.GET)
     public String seeUserWorkouts(HttpSession session, Model model) {
-        return "";
+        User sessionUser = (User) session.getAttribute("LoggedInUser");
+        System.out.println(sessionUser.getUserFitnessInfo());
+        System.out.println("Staður1");
+        if (sessionUser == null){
+            model.addAttribute("workoutType", "ekkert1");
+            System.out.println("Staður2");
+            return "home";
+        }
+        if(sessionUser.getUserFitnessInfo() == null){
+            model.addAttribute("userFitnessInfo", new UserFitnessInfo());
+            model.addAttribute("workoutType", "ekkert2");
+            System.out.println("Staður3");
+            return "workout";
+        }else{
+            WorkoutType workoutType = sessionUser.getUserFitnessInfo().getWorkoutType();
+            Difficulty workoutDifficulty = sessionUser.getUserFitnessInfo().getDifficulty();
+            System.out.println("Staður4");
+            System.out.println(workoutDifficulty);
+            if(workoutType == null && workoutDifficulty == null){
+                model.addAttribute("workoutType", workoutDifficulty);
+            }
+            model.addAttribute("workoutType", workoutDifficulty);
+        }
+        System.out.println("Staður5");
+        return "workout";
     }
 
     @RequestMapping(value = "/work3", method = RequestMethod.GET)
